9267592614c2068d847b49463f0423204319450d
giorgiabes <abesadze.george@gmail.com>
2024-05-29 16:18:16 +0400

    add blogs controller

--- backand/app.js
+++ backand/app.js
@@ -2,49 +2,14 @@ const config = require("./utils/config");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
 const mongoose = require("mongoose");
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-blogSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl);
 
 app.use(cors());
 app.use(express.json());
-
-app.get("/", (request, response) => {
-  response.send("<h1>Hello, world!</h1>");
-});
-
-// list all blog posts
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-// create one blog post
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
 module.exports = app;
---
+++ backand/controllers/blogs.js
@@ -0,0 +1,37 @@
+const blogsRouter = require("express").Router();
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+// list all blog posts
+blogsRouter.get("/", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+// create one blog post
+blogsRouter.post("/", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+module.exports = blogsRouter;
