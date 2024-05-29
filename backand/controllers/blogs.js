38eba07185eec1ff09ba0a6d245199ae480d09b4
giorgiabes <abesadze.george@gmail.com>
2024-05-29 16:26:05 +0400

    4.2 Blog List, step 2

--- backand/controllers/blogs.js
+++ backand/controllers/blogs.js
@@ -1,22 +1,6 @@
 const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
 const mongoose = require("mongoose");
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-blogSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
 
 // list all blog posts
 blogsRouter.get("/", (request, response) => {
