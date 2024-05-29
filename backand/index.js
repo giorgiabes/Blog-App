1f6c33c4cfd77fc8b79cd66b0e01398f0439c4da
giorgiabes <abesadze.george@gmail.com>
2024-05-29 15:58:06 +0400

    add app.js

--- backand/index.js
+++ backand/index.js
@@ -1,53 +1,6 @@
-const express = require("express");
-const app = express();
+const app = require("./app");
 const config = require("./utils/config");
-const cors = require("cors");
-const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-blogSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-
-const mongoUrl = config.MONGODB_URI;
-mongoose.connect(mongoUrl);
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (request, response) => {
-  response.send("<h1>Hello, world!</h1>");
-});
-
-// list all blog posts
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-// create one blog post
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = config.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
 });
