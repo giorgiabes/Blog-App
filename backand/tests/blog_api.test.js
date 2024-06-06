ef7b694a6b0c8d63ffe8cf56accee05c4f7fe0e7
giorgiabes <abesadze.george@gmail.com>
2024-06-06 10:41:09 +0400

    4.9: Blog List Tests, step 2

--- backand/tests/blog_api.test.js
+++ backand/tests/blog_api.test.js
@@ -6,16 +6,23 @@ const app = require("../app");
 
 const api = supertest(app);
 
-test.only("blogs are returned as json", async () => {
+test("blogs are returned as json", async () => {
   await api
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
 });
 
-test.only("there are two blogs", async () => {
+test("there are two blogs", async () => {
   const response = await api.get("/api/blogs");
   assert.strictEqual(response.body.length, 2);
+});
+
+test("unique identifier is named id", async () => {
+  const response = await api.get("/api/blogs");
+  const property = Object.keys(response._body[0]);
+  const identifierProperty = property[property.length - 1];
+  assert.strictEqual(identifierProperty, "id");
 });
 
 after(async () => {
