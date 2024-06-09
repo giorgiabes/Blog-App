d73b4eed235ff5a8d84a45ad4c8833f0b76a6167
giorgiabes <abesadze.george@gmail.com>
2024-06-09 19:00:36 +0400

    4.17: Blog List Expansion, step 5

--- backand/controllers/blogs.js
+++ backand/controllers/blogs.js
@@ -1,34 +1,32 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
-const mongoose = require("mongoose");
+const User = require("../models/user");
 
 // list all blog posts
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
-// blogsRouter.get("/", (request, response) => {
-//   Blog.find({}).then((blogs) => {
-//     response.json(blogs);
-//   });
-// });
-
 // create one blog post
 blogsRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
+  const body = request.body;
+
+  const user = await User.findById(body.userId);
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user.id,
+  });
 
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
   response.status(201).json(savedBlog);
 });
-
-// blogsRouter.post("/", (request, response) => {
-//   const blog = new Blog(request.body);
-
-//   blog.save().then((result) => {
-//     response.status(201).json(result);
-//   });
-// });
 
 // delete a single blog post
 blogsRouter.delete("/:id", async (request, response) => {
